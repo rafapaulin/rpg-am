aa0b3c67517a34837efda821eb11a50bf57cefd2
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2016-02-03 18:02:46 -0200

    spellShema v1.0

---
+++ schemas/charSheetSchema.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var blogSchema = new Schema({
+	charName:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	:,
+	
+});
---
+++ schemas/skillSchema.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports.skillSchema = new Schema({
+	name: {type:String, required: true, minlength: 3},
+	ability: {type:String, required: true, minlength: 3},
+	desc: {type:String, required: true, minlength: 3},
+});
